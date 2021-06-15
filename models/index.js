7ddf33053b0196d21416ac2986b7126f1d5a8b1f
APB98 <borja.basolutions@gmail.com>
2021-06-14 21:16:56 -0500

    changes on the index file creation of associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  //On delete maybe has to go somewhere else
+  //onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: {
+    model: ProductTag
+  }
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: ProductTag
+});
+
 
 module.exports = {
   Product,
